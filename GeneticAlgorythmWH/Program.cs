8b86d37e114f4617c56a464ee0b4e768eb85da5a
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-05-17 23:01:46 +0200

    Created Initial Warehouse  - Warehouse is list of shelfs - Generating initial data for the shelfs

--- GeneticAlgorythmWH/Program.cs
+++ GeneticAlgorythmWH/Program.cs
@@ -2,11 +2,26 @@
 {
     internal class Program
     {
-        int populationSize = 10;
         
-
         private static void Main(string[] args)
-        {
+        {        
+            int populationSize = 10;
+            int shelfCount = 6;
+
+            Random random = new Random();
+
+            List<Shelf[]> population = new List<Shelf[]>();
+    
+
+            Shelf[] Warehouse = new Shelf[shelfCount];
+            for (int i = 0;i < shelfCount; i++)
+            {
+                Warehouse[i].Id = i;
+                Warehouse[i].Usage = random.Next(0, 100);
+                Warehouse[i].Coordinates = new Point(random.Next(0,100),random.Next(0,100));
+            }
+
+            
             
         }
     }
