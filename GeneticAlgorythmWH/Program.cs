b20e8d562526ae1aa0279f437bced60af8cd69c8
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-05-18 12:57:37 +0200

    Relieved population generation to another file

--- GeneticAlgorythmWH/Program.cs
+++ GeneticAlgorythmWH/Program.cs
@@ -2,33 +2,14 @@
 {
     internal class Program
     {
-
         private static void Main(string[] args)
-        {        
-            int populationSize = 10;
-            int shelfCount = 6;
-
-            Random random = new Random();
-
+        {
             List<Warehouse> population = new List<Warehouse>();
-    
-            Warehouse warehouse= new Warehouse(shelfCount);
 
-            population.Add(warehouse);
-
-            for (int i = 0;i<populationSize;i++)
-            {
-                population.Add(new Warehouse(6));
-                population[i].ShuffleShelvs();    
-            }
-
-            foreach (Warehouse wh in population)
-            {
-                Console.WriteLine(wh+"\n");
-            }
+            population.GeneratePopulation(10,6);
 
 
-
+            
             
             
         }
