cdde9a4d08f60f6943695f4b1a220ce0d0a1296a
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-05-21 22:02:09 +0200

    Main now generating population and showing warehouse value

--- GeneticAlgorythmWH/Program.cs
+++ GeneticAlgorythmWH/Program.cs
@@ -6,15 +6,15 @@
         {
             List<Warehouse> population = new List<Warehouse>();
 
-            population.GeneratePopulation(10,6);
+            population.GeneratePopulation(10, 6);
 
 
-            foreach(var warehouse in population){
-                Console.WriteLine("Warehouse: {0}",warehouse.GetWarehouseValue());
+            foreach (var warehouse in population)
+            {
+                Console.WriteLine("Warehouse: {0}", warehouse.GetWarehouseValue());
             }
 
-            
+
         }
     }
 }
-    
