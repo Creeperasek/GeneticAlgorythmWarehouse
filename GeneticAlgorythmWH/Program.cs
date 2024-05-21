2c8237e50d31216eb979d927e3f6cc8e1e36d60e
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-05-21 22:20:27 +0200

    Removed Code from program.cs - Creating setup for new class genetic algorythm for easier handling of tournament and mutation

--- GeneticAlgorythmWH/Program.cs
+++ GeneticAlgorythmWH/Program.cs
@@ -4,17 +4,8 @@
     {
         private static void Main(string[] args)
         {
-            List<Warehouse> population = new List<Warehouse>();
-
-            population.GeneratePopulation(10, 6);
-
-
-            foreach (var warehouse in population)
-            {
-                Console.WriteLine("Warehouse: {0}", warehouse.GetWarehouseValue());
-            }
-
-
+            
+            
         }
     }
 }
