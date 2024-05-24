6ec7d0f430121074192f3d6a71cdca8b07aca3cb
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-05-24 11:38:08 +0200

    Program cs now have example of genetic algorithm

--- GeneticAlgorythmWH/Program.cs
+++ GeneticAlgorythmWH/Program.cs
@@ -1,11 +1,27 @@
-﻿namespace GeneticAlgorythmWH
+﻿using System;
+
+namespace GeneticAlgorythmWH
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            
-            
+            GeneticAlgorithm algorithm = new GeneticAlgorithm(10, 2, 0.1, 50);
+
+            for (int i = 0; i < 10; i++)
+            {
+                algorithm.EvolvePopulation();
+                List<Warehouse> population = algorithm.Population;
+
+                Console.WriteLine($"GEN {i}:\n");
+                var warehouses = population.OrderByDescending(w => w.GetWarehouseValue()).First();
+                // foreach (var warehouse in warehouses){
+                Console.WriteLine($"{warehouses}");
+                // }
+                
+                Console.WriteLine("\n");
+            }
+
         }
     }
 }
